82c669e7d8e90f2afb23fadbc9b19f5dba29b7a0
zoomiboomi <27johnballenger+3@ucsstudent.org>
2025-10-15 11:26:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,45 +1,22 @@
 import { MainMenu } from './engine/mainMenu.js';
-import { Display } from './engine/display.js';
-import { Input } from './engine/input.js';
-import { World } from './engine/world.js';
-import { Player } from './engine/player.js';
 
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
-function resize() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-}
-window.addEventListener('resize', resize);
-resize();
-
 let gameStarted = false;
 
-// Game objects - created later only if game starts
-let display, input, world, player;
-
-const mainMenu = new MainMenu(canvas, ctx, startGame);
-
-function startGame() {
+const mainMenu = new MainMenu(canvas, ctx, () => {
   gameStarted = true;
-  display = new Display('gameCanvas');
-  input = new Input();
-  world = new World();
-  player = new Player();
-}
+  console.log('Game would start here');
+  // TODO: replace with game initialization
+});
 
 function animate() {
   if (!gameStarted) {
     mainMenu.update();
     mainMenu.render();
-  } else {
-    input.update();
-    player.update(world, input);
-    world.update();
-    display.render(world, player);
   }
   requestAnimationFrame(animate);
 }
 
-requestAnimationFrame(animate);
+animate();
