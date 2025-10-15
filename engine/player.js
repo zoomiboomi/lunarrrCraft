20427db363958a40f84da09230e943967a820fb7
zoomiboomi <27johnballenger+3@ucsstudent.org>
2025-10-15 10:52:11 -0400

    Update player.js

--- engine/player.js
+++ engine/player.js
@@ -1,1 +1,23 @@
+export class Player {
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    this.z = 0;
+  }
 
+  update(world, input) {
+    // TODO: update player position based on input & physics
+    if (input.isKeyDown('KeyW')) {
+      this.z -= 0.1;
+    }
+    if (input.isKeyDown('KeyS')) {
+      this.z += 0.1;
+    }
+    if (input.isKeyDown('KeyA')) {
+      this.x -= 0.1;
+    }
+    if (input.isKeyDown('KeyD')) {
+      this.x += 0.1;
+    }
+  }
+}
