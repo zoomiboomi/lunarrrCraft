2b6751bb8101687da9cb9864e5aca6c56114e806
zoomiboomi <27johnballenger+3@ucsstudent.org>
2025-10-15 08:39:25 -0400

    Create maingameloop.java

---
+++ maingameloop.java
@@ -0,0 +1,27 @@
+package com.lunarrrcraft.engine;
+
+import org.lwjgl.opengl.GL11;
+
+public class MainGameLoop {
+
+    private DisplayManager displayManager;
+
+    public void start() {
+        displayManager = new DisplayManager();
+        displayManager.createDisplay(1280, 720, "LunarrrCraft");
+
+        while (!displayManager.shouldClose()) {
+            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+
+            // TODO: render blocks and player
+
+            displayManager.update();
+        }
+
+        displayManager.close();
+    }
+
+    public static void main(String[] args) {
+        new MainGameLoop().start();
+    }
+}
