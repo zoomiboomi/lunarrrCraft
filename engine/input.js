7f25b55a053e9560b16617b7ed46397ccf46216b
zoomiboomi <27johnballenger+3@ucsstudent.org>
2025-10-15 10:51:40 -0400

    Update input.js

--- engine/input.js
+++ engine/input.js
@@ -1,1 +1,16 @@
+export class Input {
+  constructor() {
+    this.keys = {};
 
+    window.addEventListener('keydown', (e) => this.keys[e.code] = true);
+    window.addEventListener('keyup', (e) => this.keys[e.code] = false);
+  }
+
+  update() {
+    // Could track toggles or mouse later
+  }
+
+  isKeyDown(keyCode) {
+    return !!this.keys[keyCode];
+  }
+}
